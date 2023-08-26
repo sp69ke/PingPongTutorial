74ed5561a5c4cb59e8728ae6546faf39fad59065
sp69ke <2351587229@qq.com>
2023-08-26 13:16:04 +0800

    feat: add Message

---
+++ PingPongTutorial/src/Message.cpp
@@ -0,0 +1,26 @@
+#include <easyx.h>
+#include "../inc/Message.h"
+
+static Coordinate mouse;
+
+void PeekInputMessage()
+{
+	ExMessage msg;
+
+	while (peekmessage(&msg, EX_MOUSE))
+	{
+		if (msg.message == WM_MOUSEMOVE)
+		{
+			mouse.x = msg.x;
+			mouse.y = msg.y;
+		}
+	}
+}
+bool IsKeyDown(int key)
+{
+	return (GetAsyncKeyState(key) & 0x8000);
+}
+Coordinate GetMouseCoordinate()
+{
+	return mouse;
+}
